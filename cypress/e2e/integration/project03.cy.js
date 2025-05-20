3e3ab8a29f9dd20631ede11a431ebaa6dc070d7d
HAJEEMOH000 <mohammadnhajeer@gmail.com>
2025-05-20 15:23:47 -0500

    Project 3

--- cypress/e2e/integration/project03.cy.js
+++ cypress/e2e/integration/project03.cy.js
@@ -2,15 +2,56 @@
 
 import { BookingPage } from "../../../pages/BookingPage"
 
+let stateAbbr = {}
 describe('Project 03', () => {
     beforeEach(() => {
         cy.visit(`${Cypress.env("SITE_URL")}/frontend/booking`)
         cy.fixture('example').then(function(data) {
             this.triptype = data.triptype
         })
+        cy.fixture('stateAbbr').then(function(data) {
+            stateAbbr = data
+        })
     })
 
     const bookingPage = new BookingPage()
+
+    const assertElementsAreVisible = elements => elements.forEach(el => el.should('be.visible'))
+
+    const elementsToBeVisible = () => [
+        bookingPage.getCabinClassLabel(),
+        bookingPage.getCabinClassDropdown(),
+        bookingPage.getFromLabel(),
+        bookingPage.getFromDropdown(),
+        bookingPage.getToLabel(),
+        bookingPage.getToDropDown(),
+        bookingPage.getDepartLabel(),
+        bookingPage.getDepartDatePicker(),
+        bookingPage.getReturnLabel(),
+        bookingPage.getReturnDatePicker(),
+        bookingPage.getNumberOfPassengersLabel(),
+        bookingPage.getNumberOfPassengersDropDown(),
+        bookingPage.getPassenger1Label(),
+        bookingPage.getPassenger1Dropdown()
+        ]
+
+    const assertNumberOfPassengersDefault = () => {
+        bookingPage.getNumberOfPassengersDropDown()
+            .find('option:selected')
+            .should('have.value', '1')
+    }
+
+    const assertPassengerTypeDefault = () => {
+        bookingPage.getPassenger1Dropdown()
+            .find('option:selected')
+            .should('have.value', 'Adult (16-64)')
+    }
+
+    const assertBookButtonIsVisibleAndEnabled = () => {
+        bookingPage.getBookButton()
+            .should('be.visible')
+            .and('be.enabled')
+    }
 
     it('Test Case 01 - Validate the default Book your trip form', function() {
         bookingPage.getTripType().each(($el, index) => {
@@ -22,31 +63,165 @@ describe('Project 03', () => {
             cy.wrap($el).should('have.text', this.triptype[index])
         })
 
+        assertElementsAreVisible(elementsToBeVisible())
+
+        bookingPage.getReturnDatePicker().should('be.disabled')
+
+        assertNumberOfPassengersDefault()
+        assertPassengerTypeDefault()
+        assertBookButtonIsVisibleAndEnabled()
     })
 
 
-    // it('Test Case 02 - Validate the Book your trip form when Round trip is selected', () => {
+    it('Test Case 02 - Validate the Book your trip form when Round trip is selected', () => {
+        bookingPage.getTripType().eq(1)
+            .realClick()
+            .find('[type="radio"]')
+            .should('be.checked')
+        
+        bookingPage.getTripType().eq(0)
+            .should('not.be.checked')
+
+        assertElementsAreVisible(elementsToBeVisible())
+        assertNumberOfPassengersDefault()
+        assertPassengerTypeDefault()
+        assertBookButtonIsVisibleAndEnabled()
+    })
+    const nextDay = new Date()
+    nextDay.setDate(nextDay.getDate() + 1)
+    const formattedDateDay = nextDay.toLocaleDateString('en-US', {
+        weekday: 'short',
+        month: 'short',
+        day: '2-digit',
+        year: 'numeric'
+    })
+    const expectedDateDay = formattedDateDay.replace(/,/g, '')
 
 
-    // })
+    const nextWeek = new Date()
+    nextWeek.setDate(nextWeek.getDate() + 7)
+    const formattedDateWeek = nextWeek.toLocaleDateString('en-US', {
+        weekday: 'short',
+        month: 'short',
+        day: '2-digit',
+        year: 'numeric'
+    })
+    const expectedDateWeek = formattedDateWeek.replace(/,/g, '')
 
 
-    // it('Test Case 03 - Validate the booking for 1 passenger and one way', () => {
+    const nextMonth = new Date(nextWeek)
+    nextMonth.setMonth(nextMonth.getMonth() + 1)
+    const formattedDateMonth = nextMonth.toLocaleDateString('en-US', {
+        weekday: 'short',
+        month: 'short',
+        day: '2-digit',
+        year: 'numeric'
+    })
+    const expectedDateMonth = formattedDateMonth.replace(/,/g, '')
 
 
-    // })
+    
+    it('Test Case 03 - Validate the booking for 1 passenger and one way', () => {
+        bookingPage.getTripType().eq(0).realClick()
+        
+        bookingPage.getCabinClassDropdown().select('Business')
+
+        bookingPage.getFromDropdown().select('Illinois')
+
+        bookingPage.getToDropDown().select('Florida')
+
+        bookingPage.getDepartDatePicker().clear().type(formattedDateWeek)
+
+        bookingPage.getNumberOfPassengersDropDown().select(0)
+
+        bookingPage.getPassenger1Dropdown().select(1)
+
+        bookingPage.getBookButton().realClick()
+
+        let from, to, passengers, passengerType, cabinClass
+
+        bookingPage.getFromDropdown().find('option:selected').invoke('text').then(text => from = stateAbbr[text])
+        bookingPage.getToDropDown().find('option:selected').invoke('text').then(text => to = stateAbbr[text])
+        bookingPage.getNumberOfPassengersDropDown().find('option:selected').invoke('text').then(text => passengers = text)
+        bookingPage.getPassenger1Dropdown().find('option:selected').invoke('text').then(text => passengerType = text)
+        bookingPage.getCabinClassDropdown().find('option:selected').invoke('text').then(text => cabinClass = text)
+        
+        cy.then(() => {
+        cy.get('.ml-3').should('contain.text', `${from} to ${to}`)
+        cy.get('.ml-3').should('contain.text', expectedDateWeek)
+        cy.get('.ml-3').should('contain.text', `Number of Passengers: ${passengers}`)
+        cy.get('.ml-3').should('contain.text', `Passenger 1: ${passengerType}`)
+        cy.get('.ml-3').should('contain.text', `Cabin class: ${cabinClass}`)
+    })
+    })
 
 
-    // it('Test Case 04 - Validate the booking for 1 passenger and round trip', () => {
+    it('Test Case 04 - Validate the booking for 1 passenger and round trip', () => {
+        bookingPage.getTripType().eq(1).realClick()
+        bookingPage.getCabinClassDropdown().select('First')
+        bookingPage.getFromDropdown().select('California')
+        bookingPage.getToDropDown().select('Illinois')
+        bookingPage.getDepartDatePicker().clear().type(formattedDateWeek)
+        bookingPage.getReturnDatePicker().clear().type(formattedDateMonth)
+        bookingPage.getNumberOfPassengersDropDown().select(0)
+        bookingPage.getPassenger1Dropdown().select(0)
+        bookingPage.getBookButton().realClick()
+
+        let from, to, passengers, passengerType, cabinClass
+
+        bookingPage.getFromDropdown().find('option:selected').invoke('text').then(text => from = stateAbbr[text])
+        bookingPage.getToDropDown().find('option:selected').invoke('text').then(text => to = stateAbbr[text])
+        bookingPage.getNumberOfPassengersDropDown().find('option:selected').invoke('text').then(text => passengers = text)
+        bookingPage.getPassenger1Dropdown().find('option:selected').invoke('text').then(text => passengerType = text)
+        bookingPage.getCabinClassDropdown().find('option:selected').invoke('text').then(text => cabinClass = text)
+        
+        cy.then(() => {
+        cy.get('.ml-3').should('contain.text', `${from} to ${to}`)
+        cy.get('.ml-3').should('contain.text', expectedDateWeek)
+        cy.get('.ml-3').should('contain.text', `Number of Passengers: ${passengers}`)
+        cy.get('.ml-3').should('contain.text', `Passenger 1: ${passengerType}`)
+        cy.get('.ml-3').should('contain.text', `Cabin class: ${cabinClass}`)
+        })
+
+        cy.then(() => {
+            const reverseFrom = to
+            const reverseTo = from
+            cy.get('.ml-3').should('contain.text', `${reverseFrom} to ${reverseTo}`)
+            cy.get('.ml-3').should('contain.text', expectedDateMonth)
+        })
+    })
 
 
-    // })
+    it('Test Case 05 - Validate the booking for 2 passengers and one way', () => {
+        bookingPage.getTripType().eq(0).realClick()
+        bookingPage.getCabinClassDropdown().select('Premium Economy')
+        bookingPage.getFromDropdown().select('New York')
+        bookingPage.getToDropDown().select('Texas')
+        bookingPage.getDepartDatePicker().clear().type(expectedDateDay)
+        bookingPage.getNumberOfPassengersDropDown().select(1)
+        bookingPage.getPassenger1Dropdown().select(0)
+        bookingPage.getPassenger2Dropdown().select(3)
+        bookingPage.getBookButton().realClick()
 
+        
+        let from, to, passengers, passengerType1, passengerType2, cabinClass
 
-    // it('Test Case 05 - Validate the booking for 2 passengers and one way', () => {
-
-
-    // })
+        bookingPage.getFromDropdown().find('option:selected').invoke('text').then(text => from = stateAbbr[text])
+        bookingPage.getToDropDown().find('option:selected').invoke('text').then(text => to = stateAbbr[text])
+        bookingPage.getNumberOfPassengersDropDown().find('option:selected').invoke('text').then(text => passengers = text)
+        bookingPage.getPassenger1Dropdown().find('option:selected').invoke('text').then(text => passengerType1 = text)
+        bookingPage.getPassenger2Dropdown().find('option:selected').invoke('text').then(text => passengerType2 = text)
+        bookingPage.getCabinClassDropdown().find('option:selected').invoke('text').then(text => cabinClass = text)
+        
+        cy.then(() => {
+        cy.get('.ml-3').should('contain.text', `${from} to ${to}`)
+        cy.get('.ml-3').should('contain.text', expectedDateDay)
+        cy.get('.ml-3').should('contain.text', `Number of Passengers: ${passengers}`)
+        cy.get('.ml-3').should('contain.text', `Passenger 1: ${passengerType1}`)
+        cy.get('.ml-3').should('contain.text', `Passenger 2: ${passengerType2}`)
+        cy.get('.ml-3').should('contain.text', `Cabin class: ${cabinClass}`)
+        })
+    })
 
 
 
--- pages/BookingPage.js
+++ pages/BookingPage.js
@@ -6,4 +6,71 @@ export class BookingPage extends BasePage {
         return cy.get('.radio')
     }
 
+    getCabinClassLabel() {
+       return cy.get('.label').eq(1)
+    }
+
+    getCabinClassDropdown() {
+        return cy.get('.select').eq(0).find('select')
+    }
+
+    getFromLabel() {
+        return cy.get('.label').eq(2)
+    }
+
+    getFromDropdown() {
+        return cy.get('.select').eq(1).find('select')
+    }
+
+    getToLabel() {
+        return cy.get('.label').eq(3)
+    }
+
+    getToDropDown() {
+        return cy.get('.select').eq(2).find('select')
+    }
+
+    getDepartLabel() {
+        return cy.get('.label').eq(4)
+    }
+
+    getDepartDatePicker() {
+        return cy.get('[type="text"]').eq(0)
+    }
+
+    getReturnLabel() {
+        return cy.get('.label').eq(5)
+    }
+
+    getReturnDatePicker() {
+        return cy.get('[type="text"]').eq(1)
+    }
+
+    getNumberOfPassengersLabel() {
+        return cy.get('.label').eq(6)
+    }
+
+    getNumberOfPassengersDropDown() {
+        return cy.get('.select').eq(3).find('select')
+    }
+
+    getPassenger1Label() {
+        return cy.get('.label').eq(7)
+    }
+
+    getPassenger1Dropdown() {
+        return cy.get('.select').eq(4).find('select')
+    }
+
+    getBookButton() {
+        return cy.get('.Button_c_button__TmkRS')
+    }
+
+    getBookingInfo() {
+        return cy.get('.ml-3')
+    }
+
+    getPassenger2Dropdown() {
+        return cy.get('.select').eq(5).find('select')
+    }
 }
